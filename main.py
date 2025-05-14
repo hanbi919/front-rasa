e03472dd496205c2024d2ddc5cfd45c443d6cc67
hanbi919 <hanbi918@163.com>
2025-05-14 15:50:43 +0800

    added

--- main.py
+++ main.py
@@ -251,4 +251,4 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=5678, workers=8,  # 根据 CPU 核心数调整
                 limit_concurrency=1000,  # 最大并发连接数
-                timeout_keep_alive=30,  # Keep-Alive 超时时间)
+                timeout_keep_alive=30,)  # Keep-Alive 超时时间)
